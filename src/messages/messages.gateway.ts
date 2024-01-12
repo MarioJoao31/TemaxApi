837025c3066a7e7e34115a67992d883b368d972e
pinante <Mario.joao31@hotmail.com>
2024-01-12 11:10:50 +0000

    update handle disconnect

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -24,6 +24,12 @@ export class MessagesGateway {
     console.log(`Client connected: ${client.id}`);
   }
 
+  // quando desconectar mandar mensagem
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    // Optional: Handle disconnect logic here
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
   @SubscribeMessage('createChannel')
   handleCreateChannel(
     @MessageBody() userIDs: { userID1: string; userID2: string },
@@ -46,26 +52,29 @@ export class MessagesGateway {
     if (createMessageDto.channelID) {
       // Check if the channel exists
       const channel = this.channels.get(createMessageDto.channelID);
-  
+
       if (channel) {
         // Update channelMessages with the new message
-        const channelData = this.channelMessages.get(createMessageDto.channelID) || { messages: [] };
+        const channelData = this.channelMessages.get(
+          createMessageDto.channelID,
+        ) || { messages: [] };
         channelData.messages.push({
           channelID: createMessageDto.channelID,
           name: createMessageDto.name,
           text: createMessageDto.text,
+          senderId: client.id,
         });
         this.channelMessages.set(createMessageDto.channelID, channelData);
-  
+
         // Send the message to clients in the channel
         this.sendToChannel(createMessageDto.channelID, 'new message', {
           message: createMessageDto,
           senderId: client.id,
         });
         console.log('mensagem enviada para o channel:', createMessageDto);
-        client.emit('new message', createMessageDto);
+        //client.emit('new message', createMessageDto);
       } else {
-        console.log(`Channel ${createMessageDto.channelID} does not exist.`);
+        console.log(`Channel ${createMessageDto.channelID} does not exist2.`);
         client.emit('error', 'erro canal não existe');
       }
     } else {
@@ -99,9 +108,9 @@ export class MessagesGateway {
     const channelId = `${userIDs.userID1}-${userIDs.userID2}`;
     this.channels.set(channelId, new Set());
 
-     // Update channelMessages to include messages property
+    // Update channelMessages to include messages property
     this.channelMessages.set(channelId, { messages: [] });
-  
+
     const clients: Socket[] = [];
     for (const userID of Object.values(userIDs)) {
       const client = this.server.sockets.sockets.get(userID);
@@ -111,15 +120,16 @@ export class MessagesGateway {
         this.channels.get(channelId).add(client);
       }
     }
-  
+
     console.log(`Channel created: ${channelId}`);
   }
 
-  private sendToChannel(channelId: string, event: string, data: any) {
-    const channel = this.channels.get(channelId);
+  private sendToChannel(channelID: string, event: string, data: any) {
+    const channel = this.channels.get(channelID);
 
-    if (channel) {
-      channel.forEach((member) => member.emit(event, data));
+    if (channel && channelID) {
+      // Emitir o evento para todos os clientes no canal
+      this.server.to(channelID).emit(event, data);
     }
   }
 
@@ -128,25 +138,84 @@ export class MessagesGateway {
     @MessageBody() createChannelDto: CreateChannelDto,
     @ConnectedSocket() client: Socket,
   ) {
-    console.log(`Received getChannelMessages request for channel: ${createChannelDto.channelID}`);
+    console.log(
+      `Received getChannelMessages request for channel: ${createChannelDto.channelID}`,
+    );
     // Check if the channel exists
     const channel = this.channels.get(createChannelDto.channelID);
 
     if (channel) {
-
       console.log(this.channelMessages);
 
       // Retrieve and send the messages for the specified channel
-      const messages = this.channelMessages.get(createChannelDto.channelID) || [];
+      const messages =
+        this.channelMessages.get(createChannelDto.channelID) || [];
       const temp = createChannelDto.channelID;
-      
+
       //manda mensagens do canal
-      client.emit('channelMessages',  messages );
+      client.emit('channelMessages', messages);
+
+      console.log('messages:', messages);
+
       console.log(
         `Sent messages from channel ${createChannelDto.channelID} to client ${client.id}`,
       );
     } else {
       console.log(`Channel ${createChannelDto.channelID} does not exist.`);
     }
+  }
+
+  @SubscribeMessage('getUserChannels')
+  getUserChannels(
+    @MessageBody() { userID }: { userID: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const userChannels: string[] = [];
+
+    console.log(`Checking channels for user ${userID}`);
+
+    this.channels.forEach((channel, channelId) => {
+      if (
+        channelId.endsWith(`-${userID}`) ||
+        channelId.startsWith(`${userID}-`)
+      ) {
+        console.log(`User ${userID} is in channel ${channelId}`);
+        userChannels.push(channelId);
+      }
+    });
+
+    console.log(`Channels for user ${userID}:`, userChannels);
+
+    client.emit('userChannels', userChannels);
+    console.log(`Sent channels for user ${userID} to client ${client.id}`);
+  }
+
+  @SubscribeMessage('joinChannel')
+  handleJoinChannel(
+    @MessageBody() payload: { channelID: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    // Validate and sanitize the channelId
+    const sanitizedChannelId = this.validateAndSanitizeChannelId(
+      payload.channelID,
+    );
+
+    // Check if the channel exists
+    const channel = this.channels.get(sanitizedChannelId);
+
+    if (channel) {
+      // Join the channel
+      client.join(sanitizedChannelId);
+      console.log(`User ${client.id} joined channel ${sanitizedChannelId}`);
+    } else {
+      console.log(`Channel ${sanitizedChannelId} does not exist.`);
+      client.emit('error', 'Channel does not exist');
+    }
+  }
+
+  private validateAndSanitizeChannelId(channelId: string): string {
+    // Implement your validation and sanitization logic here
+    // For simplicity, assuming channelId is valid as it is
+    return channelId;
   }
 }
